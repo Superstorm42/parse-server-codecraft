45883eef333e4ca775edf9ad509f3dfddc6a0b14
Riasat Ali <riasatali4242@gmail.com>
2021-12-21 17:47:30 +0600

    db path updated

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
+var express = require("express");
+var ParseServer = require("parse-server").ParseServer;
 // var S3Adapter = require('parse-server').S3Adapter;
-var path = require('path');
+var path = require("path");
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-	console.log('DATABASE_URI not specified, falling back to localhost.');
+	console.log("DATABASE_URI not specified, falling back to localhost.");
 }
 
 var api = new ParseServer({
 	//**** General Settings ****//
 
-	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-	
+	databaseURI:
+		"mongodb://sky4242:storm4242@localhost:27017/parse-dev?authSource=admin",
+	cloud: process.env.CLOUD_CODE_MAIN || __dirname + "/cloud/main.js",
+	serverURL: process.env.SERVER_URL || "http://localhost:1337/parse", // Don't forget to change to https if needed
+
 	//**** Security Settings ****//
-	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false, 
-	appId: process.env.APP_ID || 'myAppId',
-	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!	
-	
+	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false,
+	appId: process.env.APP_ID || "101",
+	masterKey: process.env.MASTER_KEY || "202", //Add your master key here. Keep it secret!
+
 	//**** Live Query ****//
 	// liveQuery: {
 	// 	classNames: ["TestObject", "Place", "Team", "Player", "ChatMessage"] // List of classes to support for query subscriptions
@@ -34,7 +35,7 @@ var api = new ParseServer({
 	/* Set the mount path as it is in serverURL */
 	// publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
 	/* This will appear in the subject and body of the emails that are sent */
-	// appName: process.env.APP_NAME || "CodeCraft", 
+	// appName: process.env.APP_NAME || "CodeCraft",
 
 	// emailAdapter: {
 	// 	module: 'parse-server-simple-mailgun-adapter',
@@ -44,7 +45,7 @@ var api = new ParseServer({
 	// 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
 	// 	}
 	// },
-	
+
 	//**** File Storage ****//
 	// filesAdapter: new S3Adapter(
 	// 	{
@@ -59,31 +60,31 @@ var api = new ParseServer({
 var app = express();
 
 // Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || "/parse";
+console.log(api);
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function (req, res) {
-	res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get("/", function (req, res) {
+	res.status(200).send(
+		"I dream of being a website.  Please star the parse-server repo on GitHub!"
+	);
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function (req, res) {
-	res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get("/test", function (req, res) {
+	res.sendFile(path.join(__dirname, "/public/test.html"));
 });
-
-
 
 var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
+var httpServer = require("http").createServer(app);
 httpServer.listen(port, function () {
-	console.log('parse-server-example running on port ' + port + '.');
+	console.log("parse-server-example running on port " + port + ".");
 });
-
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
